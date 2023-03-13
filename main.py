79da71c5c85e099662f4616526b1dafb0e2ffd5e
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2023-03-13 08:24:32 -0500

    cli version done

--- main.py
+++ main.py
@@ -1,32 +1,26 @@
-import random, time, sys
+import random, time
 from tkinter import *
-
-
-window = Tk()
-window.title = "Makinator"
-pressed = StringVar()
-
 def main():
-    pressed = StringVar()
     random.seed(time.time())
     numbers = [i for i in range(1, 1000)]
-    text.config(text="Think of a number between 1-1000")
+    print("Think of a number between 1-1000")
     i = 2
     d = int(len(numbers)//2)
-    operation = 0
+    skip = 10
     while not len(numbers) <= 1:
-        if (random.random() > 0.5):
-            text.config(text=f'Is your number less than {d}?\n')
-            optionButton.wait_variable(check)
-            numbers = list(filter(lambda x: (x < d) if option else (x > d), numbers))
-            d = int(d//2) if option else int(d*1.5)
+        if (random.random() > 0.5) and 0 < len(list(filter(lambda x: (x < d), numbers))) <= int(len(numbers)/2):
+            q = getInput(f'Is your number less than {d}?\n', str, ['y', 'n'])
+            numbers = list(filter(lambda x: ((x < d) if q == 'y' else (x > d)), numbers))
+            skip *= -1 if q == 'y' else abs(skip/skip)
+            d = (int(d//2) if q  == 'y' else int(d*1.2))
+            print(numbers)
         elif 0 < len(list(filter(lambda x: x % i == 0, numbers))) <= int(len(numbers)/2):
-            text.config(text=f'Is your number divisible by {i}?\n')
-            optionButton.wait_variable(check)
-            #q = getInput(f'Is your number divisible by {i}?\n', str, ['y', 'n'])
-            numbers = list(filter(lambda x:( x % i == 0) if option else (not x % i==0), numbers))
+            q = getInput(f'Is your number divisible by {i}?\n', str, ['y', 'n'])
+            numbers = list(filter(lambda x: (( x % i == 0) if q == 'y' else (not x % i==0)), numbers))
+            print(numbers)
         i += 1
-    text.config(text=f'Your number is {numbers[0]}!')
+        d+=10
+    print(f'Your number is {numbers[0]}!')
     
 
 def getInput(question, t, expected):
@@ -40,19 +34,6 @@ def getInput(question, t, expected):
         ans = t(ans)
     return ans
 
-text = Label(window, text="QUESTION")
-text.pack()
-option = BooleanVar()
-check = StringVar()
-optionYes = Radiobutton(window, text="Yes", variable=option, value=True)
-optionNo = Radiobutton(window, text="No", variable=option, value=False)
-optionQuit = Button(window, text="Quit", command=lambda: window.destroy())
-optionYes.pack(side="left")
-optionNo.pack(side="right")
-optionQuit.pack(side="top")
-optionButton = Button(window, text="Enter", command=lambda: check.set(str(time.time())))
-optionButton.pack(side="top")
-window.after(0, main)
-window.mainloop()
-# if "__main__" == __name__:
-#     main()
+
+if "__main__" == __name__:
+    main()
