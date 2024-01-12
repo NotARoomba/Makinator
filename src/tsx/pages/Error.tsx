d36f031020173ccc0c3318a938586699510d8ca3
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2024-01-11 20:45:02 -0500

    yet

--- src/tsx/pages/Error.tsx
+++ src/tsx/pages/Error.tsx
@@ -8,7 +8,7 @@ export default function Error() {
         <div className="m-auto align-middle justify-center h-full flex flex-col">
           <p className="text-center text-8xl my-3 font-bold">404</p>
           <p className="text-2xl md:text-4xl font-semibold">
-            This page does not exist!
+            This page does not exist! (yet...)
           </p>
           <div className="mx-auto">
             <NavButton text="Home" route="/" />
