3f85d28817b92ed021d411cfff6f7180e48a99c2
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2024-01-13 10:56:35 -0500

    some rounding

--- src/tsx/components/misc/Credits.tsx
+++ src/tsx/components/misc/Credits.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Credits() {
   return (
-    <div className="absolute flex justify-center align-middle bottom-0 left-1/2  -translate-x-1/2 text-text bg-background/60">
+    <div className="absolute flex justify-center align-middle bottom-0 left-1/2  -translate-x-1/2 text-text bg-background/60 rounded-t-xl">
       <Link
         className="h-min max-h-fit m-2 hover:underline"
         to="https://notaroomba.dev"
